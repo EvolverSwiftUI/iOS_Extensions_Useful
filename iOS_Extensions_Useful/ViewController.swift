c426515f98c3b8d2dd80af48ee2a4fe4715366d4
Sivaram Yadav <sivaramaiah.nakka@concentrix.com>
2022-05-04 14:24:04 +0530

    addons

--- iOS_Extensions_Useful/ViewController.swift
+++ iOS_Extensions_Useful/ViewController.swift
@@ -7,13 +7,34 @@
 
 import UIKit
 
+struct Book {
+    var title: String
+    var author: String
+    var price: Double
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        iOS_Useful_Extensions_Usage()
     }
 
+    func iOS_Useful_Extensions_Usage() {
+        
+        let boolValue = "Test".isNotEmpty
+        debugPrint(boolValue)
+        
+        
+        var books = [Book]()
+        let book = Book(title: "Title", author: "Author", price: 100.00)
+        books.append(book)
+        let boolValue2 = books.isOneAndOnly
+        debugPrint(boolValue2)
+        
+    }
 
 }
 
---
+++ iOS_Extensions_Useful/iOS_Useful+Extensions.swift
@@ -0,0 +1,13 @@
+//
+//  iOS_Useful+Extensions.swift
+//  iOS_Extensions_Useful
+//
+//  Created by Sivaram Yadav on 5/4/22.
+//
+
+import Foundation
+
+extension Collection {
+    var isNotEmpty: Bool { !isEmpty }
+    var isOneAndOnly: Bool { count == 1 }
+}
